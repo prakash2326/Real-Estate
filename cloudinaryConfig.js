f07e8113bcc47e863b61d52e4a3c10d60f08642e
PRAKASHG23 <pg793882@gmail.com>
2026-02-05 04:25:15 +0530

    may be last change

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,5 +1,6 @@
-const cloudinary = require('cloudinary').v2;
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
